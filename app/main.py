19ddaaf955e3865d368a5d103a5a9c33a0cff15f
Petterson04 <ferpetterson@gmail.com>
2026-04-08 22:51:08 -0600

    Debug database

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 from fastapi import FastAPI
-from app.database import engine, Base
+from app.database import DATABASE_URL, engine, Base
 from app.routers import users, listas, productos
 from app.models import models
 
@@ -11,3 +11,11 @@ Base.metadata.create_all(bind=engine)
 app.include_router(users.router)
 app.include_router(listas.router)
 app.include_router(productos.router)
+
+print("🚀 DATABASE_URL =", DATABASE_URL)
+print ("🚀 Database connected successfully!")
+
+@app.on_event("startup")
+def startup():
+    print("⚡ STARTUP EVENT RUNNING")
+    Base.metadata.create_all(bind=engine)
