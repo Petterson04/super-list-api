5cd67f45ea6f0c0d2f463689149400d36481e520
Petterson04 <ferpetterson@gmail.com>
2026-04-09 21:29:34 -0600

    Changes in getcurrentuser

--- app/auth/dependencies.py
+++ app/auth/dependencies.py
@@ -1,20 +1,21 @@
 from fastapi import Depends, HTTPException
-from fastapi.security import HTTPBearer
+from fastapi.security import HTTPBearer, HTTPAuthorizationCredentials
 from jose import jwt, JWTError
-from app.auth.auth import SECRET_KEY, ALGORITHM
 
 security = HTTPBearer()
 
-def get_current_user(token=Depends(security)):
+def get_current_user(credentials: HTTPAuthorizationCredentials = Depends(security)):
     try:
+        token = credentials.credentials  # 👈 importante
+
         payload = jwt.decode(
-            token.credentials,
+            token,
             SECRET_KEY,
             algorithms=[ALGORITHM]
         )
-        return payload["user_id"]
 
-    except JWTError:
+        return payload.get("user_id")
+
+    except JWTError as e:
+        print("JWT ERROR:", str(e))  # 👈 debug
         raise HTTPException(401, "Token inválido")
-    
-    
